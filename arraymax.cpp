8b63ca049d2b85a7a4dfc8048d051fc5f62b67d5
devashishpandey90 <116454781+devashishpandey90@users.noreply.github.com>
2022-10-22 23:03:12 +0530

    Update arraymax.cpp

--- arraymax.cpp
+++ arraymax.cpp
@@ -1,25 +1,5 @@
-// #include<iostream>
-// using namespace std;
-// void update(int arr[], int n)
-
-// {
-//     arr[0]= 120;
-//     for(int i=0;i<3;i++)
-//     {
-//         cout<<arr[i];
-//     }
-//      cout<<endl<<"inside the main fuction\n";
-// }
-// int main(){
-// int arr[3]={1,2,3};
-// update(arr,3);
-// for(int i=0;i<3;i++)
-//     {
-//         cout<<arr[i];
-//     }
-// return 0;
-// }
-
+ QUESTION 1 - WRITE THE PROGRAM TO FIND THE MAXIMUM NUMBER AND MININMUM NUMBER THROUGH ARRAY?
+     
 #include<iostream>
 using namespace std;
 int main(){
